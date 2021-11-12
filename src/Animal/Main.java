397a2c9830b7691e33fe4fb8036c6edfc5ff8a27
Thangggg <thang.nguyenvan7594@gmail.com>
2021-11-12 14:19:53 +0700

    Animal

---
+++ src/Animal/Main.java
@@ -0,0 +1,39 @@
+package Animal;
+import Fruit.Fruit;
+import Fruit.Orange;
+import Fruit.Apple;
+
+import edible.Edible;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = new Animal[2];
+        animals[0] = new Tiger();
+        animals[1] = new Chicken();
+        for (Animal animal : animals) {
+            System.out.println(animal.makeSound());
+            if (animal instanceof Chicken) {
+                Edible edibler = (Chicken) animal;
+                System.out.println(edibler.howToEat());
+            }
+
+            if (animal instanceof Tiger){
+                Edible edible = (Tiger) animal;
+                System.out.println(edible.howToEat());
+            }
+
+        }
+        Fruit[] fruits = new Fruit[2];
+        fruits[0] = new Orange();
+        fruits[1] = new Apple();
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit.howToEat());
+        }
+
+
+    }
+
+
+
+
+}
